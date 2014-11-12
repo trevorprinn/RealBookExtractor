45744f065fb232941cd9c4cedc5c09912bf48fae
Trevor Prinn <trev@babbacom.com>
2014-11-12 14:24:13 +0000

    Switch the palette of 1bbp images if they appear to be negatives.

--- FormExtract.cs
+++ FormExtract.cs
@@ -53,6 +53,9 @@ namespace RealBookExtractor {
                 foreach (PdfPage page in pdf.Pages) {
                     try {
                         foreach (var img in page.GetImages()) {
+                            if (img.PixelFormat == System.Drawing.Imaging.PixelFormat.Format1bppIndexed) {
+                                checkNegative(img);
+                            }
                             img.Save(Path.Combine(OutFolder, string.Format("{0:000}.jpg", count++)));
                         }
                     } catch (Exception ex) {
@@ -64,6 +67,28 @@ namespace RealBookExtractor {
                 }
             }
             return true;
+        }
+
+        private void checkNegative(Image img) {
+            // 1bbp images are sometimes coming out -ve.
+            // I'm assuming that the images are black and white and just checking the top LH corner.
+            var whiteCount = 0;
+            using (var bmp = new Bitmap(img)) {
+                for (var x = 0; x < Math.Min(bmp.Width, 50); x++) {
+                    for (var y = 0; y < Math.Min(bmp.Height, 50); y++) {
+                        if (bmp.GetPixel(x, y) == Color.White) whiteCount++;
+                    }
+                }
+                // There's more white than black;
+                if (whiteCount > bmp.Width * bmp.Height / 2) return;
+            }
+
+            // Reverse the entries in the palette
+            var pal = img.Palette;
+            var entries = pal.Entries.ToArray();
+            pal.Entries.SetValue(entries[1], 0);
+            pal.Entries.SetValue(entries[0], 1);
+            img.Palette = pal;
         }
 
         private void textPdf_TextChanged(object sender, EventArgs e) {
